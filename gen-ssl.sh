3ae945bbb2b58d50960d033f509fd961f9efd793
Andrew Houts <houtsandrew@gmail.com>
2017-10-19 09:05:07 -0700

    change key permissions

--- gen-ssl.sh
+++ gen-ssl.sh
@@ -8,3 +8,5 @@ if [ -f ./cert.pem ]; then
     exit -1
 fi
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3650 -nodes -subj "/C=US/ST=California/L=Santa Clara/O=None/CN=www.example.com"
+chmod 700 key.pem cert.pem
+
