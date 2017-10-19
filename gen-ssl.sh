2fe80cf8ca2a039d87d7200f8a33b9547d5452e8
Andrew Houts <houtsandrew@gmail.com>
2017-10-19 09:04:16 -0700

    dont overwrite ssl keys

--- gen-ssl.sh
+++ gen-ssl.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+if [ -f ./key.pem ]; then
+    echo "./key.pem already exists..."
+    exit -1
+fi
+if [ -f ./cert.pem ]; then
+    echo "./cert.pem already exists..."
+    exit -1
+fi
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3650 -nodes -subj "/C=US/ST=California/L=Santa Clara/O=None/CN=www.example.com"
