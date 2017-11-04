2a3fdacc646f3bf836a2a0c90e1e50a7b7ef8890
ahouts <ahouts4@gmail.com>
2017-11-04 15:25:47 -0700

    add test script for creating users

---
+++ insertuser.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import urllib.request
+import json
+import sys
+
+if len(sys.argv) != 3:
+    print("format: ./insertuser.py username password")
+    sys.exit(-1)
+
+dat = { 'Email': sys.argv[1], 'Password': sys.argv[2]}
+req = urllib.request.Request('https://productive.ahouts.com/create_user')
+req.add_header('Content-Type', 'application/json')
+try:
+    response = urllib.request.urlopen(req, json.dumps(dat).encode())
+except urllib.request.HTTPError as e:
+    error_message = e.read()
+    print(error_message)
