ae81b552ca0bc70c10f2cc092f00af8838392ad6
ahouts <ahouts4@gmail.com>
2017-11-19 12:04:43 -0800

    add getnote script

---
+++ test-scripts/getnote.py
@@ -0,0 +1,15 @@
+#!/bin/python2
+
+import requests
+import json
+import sys
+
+if len(sys.argv) != 4:
+    print("format: ./getnote.py username password id")
+    sys.exit(-1)
+
+dat = { 'Email': sys.argv[1], 'Password': sys.argv[2]}
+req = requests.put('https://productive.ahouts.com/note/get/' + sys.argv[3], 
+                   headers={'Content-Type': 'application/json'}, 
+                   data=json.dumps(dat).encode())
+print(req.text)
