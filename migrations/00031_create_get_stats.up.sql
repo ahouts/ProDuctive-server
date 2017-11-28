27303a0d98e915ca02e0b9a7b767a49cccd03619
Andrew Houts <houtsandrew@gmail.com>
2017-11-28 12:39:26 -0800

    fix get stats sql function

--- migrations/00031_create_get_stats.up.sql
+++ migrations/00031_create_get_stats.up.sql
@@ -10,10 +10,10 @@ IS
         l_temp := getNumOfNotes() / l_numOfUsers;
         l_retStr := l_retStr || ROUND(l_temp, 2) || e;
         l_retStr := l_retStr || ' The average number of projects per user is: ';
-        l_temp := getNUmOFProjects() / l_numOfUsers;
+        l_temp := getNumOfProjects() / l_numOfUsers;
         l_retStr := l_retStr || ROUND(l_temp, 2) || e;
         l_retStr := l_retStr || ' The average number of reminders per user is ';
-        l_temp := getNUmOFProjects / l_numOfUsers;
+        l_temp := getNumOfReminders() / l_numOfUsers;
         l_retStr := l_retStr || ROUND(l_temp, 2) || e;
         RETURN l_retStr;
     END;
